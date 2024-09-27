c4f81e762f31f60699298ae35dff77529ded448c
Humberto Fierro <betfie1@gmail.com>
2024-09-27 12:21:23 -0500

    update title

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return "Welcome to the NASA SMRU Application"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
