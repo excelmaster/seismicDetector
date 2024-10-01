2d498edc9637a4b698e6bda80ccfd34eeb63d847
Humberto Fierro <betfie1@gmail.com>
2024-09-30 22:37:20 -0500

    final intro design

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/dashboard')
+def graphs():
+    return render_template("dashboard.html")
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
